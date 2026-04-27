0d288f1cc760c0d0806c1e598d563d3dbbbbc417
rajesh-data-analyst <r9095476@gmail.com>
2026-04-27 12:50:25 +0530

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,21 @@
 -- SQL queries will be added here
+-- Total Sales & Profit
+SELECT SUM(Sales) AS total_sales, SUM(Profit) AS total_profit
+FROM superstore;
+
+-- Profit by Category
+SELECT Category, SUM(Profit) AS profit
+FROM superstore
+GROUP BY Category;
+
+-- Loss making Sub-Categories
+SELECT Sub_Category, SUM(Profit) AS profit
+FROM superstore
+GROUP BY Sub_Category
+ORDER BY profit ASC;
+
+-- Discount vs Profit
+SELECT Discount, AVG(Profit) AS avg_profit
+FROM superstore
+GROUP BY Discount
+ORDER BY Discount;
